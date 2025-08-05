70d9aacce2912a0ebfe7ba046bed38bd4670704b
2yunsu <2yunsu@snu.ac.kr>
2025-08-05 15:58:04 +0900

    use_centralized_V, store_true fix

--- scripts/train_ucmec_stat_coop.sh
+++ scripts/train_ucmec_stat_coop.sh
@@ -5,7 +5,7 @@ num_landmarks=3  # UCMEC 환경에서는 불필요하지만 유지
 num_agents=10  # UCMEC 환경의 M_sim 값과 맞춰야 함
 algo="rmappo"
 exp="ucmec_stat_coop"  # 실험명 변경
-seed_max=10
+seed_max=3
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
@@ -34,5 +34,6 @@ do
     --wandb_name "2yunsu" \
     --user_name "2yunsu" \
     --use_naive_recurrent_policy \
+    --use_centralized_V \
 
 done
